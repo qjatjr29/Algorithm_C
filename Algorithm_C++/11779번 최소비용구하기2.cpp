af0b20e895022970df232f1f4939785ea019c715
beomseok <qjatjr29@khu.ac.kr>
2021-10-01 10:52:38 +0900

    Update 11779번 최소비용구하기2.cpp

--- Algorithm_C++/11779번 최소비용구하기2.cpp
+++ Algorithm_C++/11779번 최소비용구하기2.cpp
@@ -5,77 +5,77 @@
 using namespace std;
 
 int n, m;
-int a, b;
+int startCity, endCity;
 vector<vector<pair<int, int>>> adj;
-vector<vector<bool>> visited;
-int result = 987654321;
-vector<int> nodeCost;
-vector<int> route;
-int temp[1001];
-void city(int start);
+vector<bool> visited;
+int preNode[1001];
+vector<int> visitedCity;
+vector<int> result;
+void city(int idx);
 int main()
 {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
 	cin >> n >> m;
 	adj = vector<vector<pair<int, int>>>(n + 1, vector<pair<int, int>>());
-	visited = vector<vector<bool>>(n + 1, vector<bool>(n + 1, false));
-	nodeCost = vector<int>(n + 1, 987654321);
+	visited = vector<bool>(n + 1, false);
+
 	for (int i = 0; i < m; i++)
 	{
-		int start, end, cost;
-		cin >> start >> end >> cost;
-		adj[start].push_back(make_pair(end, cost));
+		int sc, dc, cost;
+		cin >> sc >> dc >> cost;
+		adj[sc].push_back(make_pair(dc, cost));
 	}
-	cin >> a >> b;
-	city(a);
-	cout << nodeCost[b] << "\n";
-	int curIndex = b;
+	cin >> startCity >> endCity;
+	city(startCity);
+
+	cout << result[endCity] << "\n";
+	visitedCity.push_back(endCity);
+	int chk = endCity;
 	while (1)
 	{
-		if (curIndex == a)
-		{
-			route.push_back(a);
-			break;
-		}
-		route.push_back(curIndex);
-		curIndex = temp[curIndex];
+		chk = preNode[chk];
+		visitedCity.push_back(chk);
+		if (chk == startCity)break;
 	}
-	int size = route.size();
-	cout << size << "\n";
-	for (int i = size - 1; i > 0; i--)
+	cout << visitedCity.size() << "\n";
+	for (int i = visitedCity.size() - 1; i > 0; i--)
 	{
-		cout << route[i] << " ";
+		cout << visitedCity[i] << " ";
 	}
-	cout << route[0] << "\n";
-
+	cout << visitedCity[0] << "\n";
 
 	return 0;
 }
 
-void city(int start)
+void city(int idx)
 {
 	priority_queue<pair<int, int>> pq;
-	pq.push(make_pair(0, start));
+
+	pq.push(make_pair(0, idx));
+	result = vector<int>(n + 1, 987654321);
 
 	while (1)
 	{
+
 		if (pq.empty())break;
+		int curNode = pq.top().second;
 		int curCost = -pq.top().first;
-		int here = pq.top().second;
-
+		if (curNode == endCity)return;
 		pq.pop();
+		if (curCost > result[curNode])continue;
 
-
-		for (int i = 0; i < adj[here].size(); i++)
+		for (int i = 0; i < adj[curNode].size(); i++)
 		{
-			int there = adj[here][i].first;
-			int nextCost = curCost + adj[here][i].second;
-			if (nodeCost[there] > nextCost)
+			int nNode = adj[curNode][i].first;
+			int nCost = adj[curNode][i].second;
+			if (result[nNode] > nCost + curCost)
 			{
-				nodeCost[there] = nextCost;
-				temp[there] = here;
-				pq.push(make_pair(-nextCost, there));
+				result[nNode] = nCost + curCost;
+				preNode[nNode] = curNode;
+				pq.push(make_pair(-result[nNode], nNode));
 			}
 		}
-
 	}
 }
